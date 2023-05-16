c25b6aefcd9ec71041e65a4521cce663535780a7
GulshanSS <sahugulshan686@gmail.com>
2023-05-16 18:19:05 +0530

    Updated: Todo Component

--- client/src/components/Todo/Todo.tsx
+++ client/src/components/Todo/Todo.tsx
@@ -1,35 +1,29 @@
 import { RiDeleteBin6Line } from "react-icons/ri";
-import {
-  BsFillArrowLeftCircleFill,
-  BsFillArrowRightCircleFill,
-} from "react-icons/bs";
+import { BsPatchCheckFill } from "react-icons/bs";
+import { RxLapTimer } from "react-icons/rx";
+import { FaPencilAlt } from "react-icons/fa";
 
 const Todo = () => {
+  const TodoStatusVariants = {
+    inProgress: "text-orange-500",
+    complete: "text-green-500",
+  };
+
   return (
     <>
-      <div className="w-96 px-5 py-2 m-2 rounded-md bg-slate-200">
-        <div className="flex justify-between">
-          <h3 className="font-bold">First Todo</h3>
-          <div className="text-xl">
-            <button className="mr-2">
-              <BsFillArrowLeftCircleFill />
-            </button>
-            <button>
-              <BsFillArrowRightCircleFill />
-            </button>
-          </div>
-        </div>
-        <p className="font-semibold text-justify my-3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-          explicabo aliquam nostrum consequuntur fugit eligendi aliquid odio
-          necessitatibus unde veritatis rerum officia facere, officiis corrupti
-          nisi sit quae aspernatur optio!
-        </p>
-        <div className="flex justify-between">
-          <span className="w-24 p-1 text-sm text-center font-bold bg-orange-200 bg-opacity-30 text-orange-600 rounded-md">
-            In-Progress
-          </span>
-          <button className="text-red-500 text-xl">
+      <div className="flex justify-between px-5 py-2 m-2 rounded-md bg-slate-200">
+        <h3 className="font-bold text-justify">First Todo</h3>
+        <div className="text-xl ml-2 flex gap-1">
+          <button className={TodoStatusVariants.inProgress}>
+            <RxLapTimer />
+          </button>
+          <button>
+            <BsPatchCheckFill className={TodoStatusVariants.complete} />
+          </button>
+          <button className="text-indigo-700">
+            <FaPencilAlt />
+          </button>
+          <button className="text-red-500">
             <RiDeleteBin6Line />
           </button>
         </div>
