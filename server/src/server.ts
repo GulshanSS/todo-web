0a5f38dcbb53157bbc3d0d4022c5125c7f1453ad
GulshanSS <sahugulshan686@gmail.com>
2023-05-20 14:05:41 +0530

    Completed: Initial Backend Setup

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app: Express = express();
+
+const PORT: string = process.env.PORT || "3000";
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
