34e203ea28e26ac85a4da229eee4a81c7fc386d1
GulshanSS <sahugulshan686@gmail.com>
2023-05-23 20:12:07 +0530

    Added: Utility functions to hash and compare string/s with the help of bcrypt package

---
+++ server/src/utils/bcrypt.ts
@@ -0,0 +1,12 @@
+import bcrypt from "bcrypt";
+
+export const hash = async (data: string): Promise<string> => {
+  return await bcrypt.hash(data, process.env.SALT_ROUNDS as string);
+};
+
+export const compare = async (
+  data: string,
+  existingData: string
+): Promise<boolean> => {
+  return await bcrypt.compare(data, existingData);
+};
