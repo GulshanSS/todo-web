cbdc799e28711adfc2b3b454b786df2a55cf3108
GulshanSS <sahugulshan686@gmail.com>
2023-05-23 18:50:41 +0530

    Updated: Added dependent relational properties to Todo interface

--- server/src/model/todo.model.ts
+++ server/src/model/todo.model.ts
@@ -1,7 +1,11 @@
+import { User } from "./user.model";
+
 export interface Todo {
   id: string;
   task: string;
   status: string;
+  userId: string;
+  owner: User;
   createdAt: Date;
   updatedAt: Date;
 }
