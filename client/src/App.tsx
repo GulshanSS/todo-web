43fb07c3719f10a8deae672dc610df8d03daf33b
GulshanSS <sahugulshan686@gmail.com>
2023-05-27 11:24:55 +0530

    Add: Routing functionality using react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,23 @@
+import Navbar from "./components/Navbar/Navbar";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import VerifyOTP from "./pages/Auth/VerifyOTP";
 import Home from "./pages/Home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <div>
-        <Login />
-        <Register />
-        <VerifyOTP />
-        <Home />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/verify-otp" element={<VerifyOTP />} />
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
