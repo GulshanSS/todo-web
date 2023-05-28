67a9d7b216afd54b04e990baca92dc8ffe1da19b
GulshanSS <sahugulshan686@gmail.com>
2023-05-28 23:27:57 +0530

    Fix: maked fields optional and added verified filed to user model

--- server/src/model/user.model.ts
+++ server/src/model/user.model.ts
@@ -1,16 +1,19 @@
 import { RefreshToken } from "@prisma/client";
 import { Todo } from "./todo.model";
+import { OTP } from "./otp.model";
 
 export interface User {
   id: string;
   email: string;
   password: string;
-  todos: Todo[];
-  refreshTokens: RefreshToken[];
+  verified?: boolean;
+  todos?: Todo[];
+  refreshTokens?: RefreshToken[];
+  opt?: OTP[];
   createdAt: Date;
   updatedAt: Date;
 }
 
 export type UserCreateInput = Pick<User, "email" | "password">;
 
-export type UserUpdateInput = Partial<UserCreateInput>;
+export type UserUpdateInput = Partial<User>;
