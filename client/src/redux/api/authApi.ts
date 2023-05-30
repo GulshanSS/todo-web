dc098f87e2f08972ec403264d25572e2fd5d57d3
GulshanSS <sahugulshan686@gmail.com>
2023-05-30 23:34:08 +0530

    Add: auth api using rtk query

---
+++ client/src/redux/api/authApi.ts
@@ -0,0 +1,100 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const BASE_URL = "http://localhost:3000";
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${BASE_URL}/api/v1/`,
+  }),
+  endpoints: (builder) => ({
+    registerUser: builder.mutation<
+      { success: boolean; userId: string; message: string },
+      {
+        username: string;
+        password: string;
+      }
+    >({
+      query(data) {
+        return {
+          url: "register",
+          method: "POST",
+          body: data,
+        };
+      },
+    }),
+    loginUser: builder.mutation<
+      {
+        success: boolean;
+        accesstoken: string;
+        refreshtoken: string;
+      },
+      {
+        username: string;
+        password: string;
+      }
+    >({
+      query(data) {
+        return {
+          url: "login",
+          method: "POST",
+          body: data,
+          credentials: "include",
+        };
+      },
+    }),
+    sendOTP: builder.mutation<
+      {
+        success: boolean;
+        message: string;
+      },
+      { userId: string }
+    >({
+      query({ userId }) {
+        return {
+          url: "send-otp",
+          method: "POST",
+          body: userId,
+        };
+      },
+    }),
+    verifyOTP: builder.mutation<
+      {
+        success: boolean;
+        accesstoken: string;
+        refreshtoken: string;
+      },
+      { otp: string; userId: string }
+    >({
+      query({ otp, userId }) {
+        return {
+          url: "verify-otp",
+          method: "POST",
+          data: { otp, userId },
+          credentials: "include",
+        };
+      },
+    }),
+    logoutUser: builder.mutation<
+      { success: boolean; message: string },
+      { refreshToken: string }
+    >({
+      query({ refreshToken }) {
+        return {
+          url: "logout",
+          method: "delete",
+          data: { refreshToken },
+          credentials: "include",
+        };
+      },
+    }),
+  }),
+});
+
+export const {
+  useRegisterUserMutation,
+  useLoginUserMutation,
+  useSendOTPMutation,
+  useVerifyOTPMutation,
+  useLogoutUserMutation,
+} = authApi;
