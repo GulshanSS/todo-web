c647d093dc7b3e5ab9ffec273986298713b44c3d
GulshanSS <sahugulshan686@gmail.com>
2023-06-01 23:58:55 +0530

    Add: cookie options for access & refresh tokens

---
+++ server/src/utils/tokenCookieOptions.ts
@@ -0,0 +1,33 @@
+import { CookieOptions } from "express";
+
+export const accessTokenCookieOptions: CookieOptions = {
+  expires: new Date(
+    Date.now() +
+      parseInt(process.env.COOKIE_ACCESS_TOKEN_EXPIRES_TIME as string) *
+        60 *
+        1000
+  ),
+  maxAge:
+    parseInt(process.env.COOKIE_ACCESS_TOKEN_EXPIRES_TIME as string) *
+    60 *
+    1000,
+  httpOnly: true,
+  sameSite: "lax",
+  secure: true,
+};
+
+export const refreshTokenCookieOptions: CookieOptions = {
+  expires: new Date(
+    Date.now() +
+      parseInt(process.env.COOKIE_REFRESH_TOKEN_EXPIRES_TIME as string) *
+        60 *
+        1000
+  ),
+  maxAge:
+    parseInt(process.env.COOKIE_REFRESH_TOKEN_EXPIRES_TIME as string) *
+    60 *
+    1000,
+  httpOnly: true,
+  sameSite: "lax",
+  secure: true,
+};
